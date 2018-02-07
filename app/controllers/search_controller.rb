17cfcdf9707def20015fbfaf828919d16bb32447
Timo Tyrrell <timomitchel23@gmail.com>
2018-02-07 10:27:37 -0700

    changed up Faraday to be get immediately

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,8 @@ class SearchController < ApplicationController
 
   def index
     zip = params["q"]
-    @conn = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=10") do |faraday|
-    faraday.headers["X-API-KEY"] = ENV["nrel_key"]
-    faraday.adapter Faraday.default_adapter
-  end
+    @conn = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?limit=1&location=80203&api_key=#{ENV["nrel_key"]}") 
+    binding.pry
+    @search = JSON.parse(@conn.body, symbolize_names: true)[:results]
   end
 end
