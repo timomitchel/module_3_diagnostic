0bff42ed65b9a4116a8ce49d17fd992ca3154b26
Timo Tyrrell <timomitchel23@gmail.com>
2018-02-07 11:02:20 -0700

    refactored into nrel service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    zip = params["q"]
-    @conn = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["nrel_key"]}&location=#{zip}&fuel_type=ELEC,LPG&radius=6.0&status=E&limit=10")
-    @search = JSON.parse(@conn.body, symbolize_names: true)[:fuel_stations]
+    @search = NrelService.new(params["q"]).stations
   end
 end
---
+++ app/services/nrel_service.rb
@@ -0,0 +1,18 @@
+class NrelService
+
+  def initialize(zip)
+    @zip = zip
+  end
+  
+  def stations
+    @search = JSON.parse(faraday_call.body, symbolize_names: true)[:fuel_stations]
+  end
+  private
+  
+  attr_reader :zip
+
+  def faraday_call
+     Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["nrel_key"]}&location=#{zip}&fuel_type=ELEC,LPG&radius=6.0&status=E&limit=10")
+  end
+
+end
